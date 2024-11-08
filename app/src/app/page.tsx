97a695b56e868682028636b18097752792133773
Jackroj <devapraveen10@gmail.com>
2024-11-08 22:24:40 +0530

    addName

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-center bg-gradient-defang">
       <h1 className="text-4xl font-bold text-white">Next.js &times; Defang</h1>
+      <h1 className="text-4xl font-bold text-white">Deva</h1>
+
     </main>
   );
 }
