b84d1254aacbfa1f946af18a31025e8585b604bd
Jackroj <devapraveen10@gmail.com>
2024-11-08 22:56:37 +0530

    addedProfileComponent

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Profile from "./Component/Profile";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen min-w-screen flex-col items-center justify-center bg-gradient-defang">
+  return (<>
+  <Profile/>
+  {/* <main className="flex min-h-screen min-w-screen flex-col items-center justify-center bg-gradient-defang">
       <h1 className="text-4xl font-bold text-white">Next.js &times; Defang</h1>
       <h1 className="text-4xl font-bold text-white">Deva</h1>
-
-    </main>
+    </main> */}
+  </>
+   
   );
 }
